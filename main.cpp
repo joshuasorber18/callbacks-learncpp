5238fb6be3901a7181b860dfdd77bb2ada0c83c2
BuildTools <unconfigured@null.spigotmc.org>
2018-05-06 23:34:01 -0400

    Randomness

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <functional>
+using namespace std;
+typedef int (*callbackFunc)(int);
+int aFunction(int x) {
+    return x;
+}
+void anotherFunction(callbackFunc somePtr) {
+    cout << somePtr(5);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-
+    anotherFunction(aFunction);
     return 0;
 }
