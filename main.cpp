35e9c2088035046cf47b32878c0700b666dd40bb
BuildTools <unconfigured@null.spigotmc.org>
2018-05-09 21:59:18 -0400

    coding quiz 7.8

--- main.cpp
+++ main.cpp
@@ -1,15 +1,47 @@
 #include <iostream>
 #include <functional>
 using namespace std;
-typedef int (*callbackFunc)(int);
-int aFunction(int x) {
-    return x;
+typedef int (*arithmeticFcn)(int, int);
+typedef struct {
+    char mathOp;
+    arithmeticFcn callback;
+} arithmeticStruct;
+
+
+int add(int x, int y) {
+    return x + y;
 }
-void anotherFunction(callbackFunc somePtr) {
-    cout << somePtr(5);
+int subtract(int x, int y) {
+    return x - y;
+}
+int multiply(int x, int y) {
+    return x * y;
+}
+int divide(int x, int y) {
+    return x / y;
+}
+int remainder(int x, int y) {
+    return x % y;
 }
 
+arithmeticStruct operations[] = {
+        {'+', add},
+        {'-', subtract},
+        {'*', multiply},
+        {'/', divide},
+        {'%', remainder}
+};
+arithmeticFcn getArithmeticFunction(char functionOperator){
+    for (arithmeticStruct &op : operations) {
+        if(op.mathOp == functionOperator) {
+            return op.callback;
+        }
+    }
+    return add;
+}
 int main() {
-    anotherFunction(aFunction);
+    int x =3;
+    int y =2;
+    cout << x % y;
     return 0;
 }
